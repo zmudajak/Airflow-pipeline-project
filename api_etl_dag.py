44d889d42eeb1864c8fa43844fae3050d37a7762
Jakub Zmuda <123254431+zmudajak@users.noreply.github.com>
2023-10-16 20:30:18 +0200

    Update api_etl_dag.py
    add comments

--- api_etl_dag.py
+++ api_etl_dag.py
@@ -1,3 +1,5 @@
+
+# import libraries and etl function
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -5,7 +7,7 @@ from airflow.utils.dates import days_ago
 from datetime import datetime
 from airflow_etl import run_api_etl
 
-
+# define default arguments for airflow DAG
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -17,17 +19,19 @@ default_args = {
     "retry_delay": timedelta(minutes=1)
 }
 
-
+# define DAG
 dag = DAG(
     "api_etl_dag",
     default_args=default_args,
     description="My etl code"
 )
 
+# define etl task in DAG
 run_etl = PythonOperator(
     task_id="complete_etl",
     python_callable=run_api_etl,
     dag=dag,
 )
 
+# run task
 run_etl
